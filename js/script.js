2a89d19cb4a90e40ea61db926c07e398a09f6839
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-10-19 17:14:20 +0200

    [added] pari o dispari script + comments

--- js/script.js
+++ js/script.js
@@ -7,25 +7,30 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 **Pari e Dispari**
 */
-//componenti html primo esercizio
-const userInput = document.querySelector('.user_input').value;
-const isPalindromoResult = document.querySelector('.isPalindromoResult');
+
+//dichiaro una variabile globale
 let reverseWord = '';
 
+//dichiaro la funzione con un parametro
 function isPalindromo(word){
-  for (i = word.length - 1; i >= 0; i--){
+  //for loop dove i prende la lunghezza del parametro fino a quando è maggiore di 0 allora decrementa
+  // cosi da partire dalla fine verso l'inizio
+  for (let i = word.length - 1; i >= 0; i--){
+    //mette l'indice di word (char per char) dentro reverseWord
     reverseWord += word[i];
   }
   console.log(reverseWord);
   console.log(word);
+
+  //la condizione che controlla se il parametro è un palindromo o no
   if (reverseWord === word) {
     console.log("palindromo");
   } else {
     console.log("Non palindromo");
   }
 }
-
-isPalindromo('ozozor');
+//chiamo la funziona con il parametro indicato
+isPalindromo('anna');
 
 
 /*
@@ -39,4 +44,38 @@ Dichiariamo chi ha vinto.
 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 */
 
-//componenti secondo esercizio
+//prendo il bottone dall'HTML
+const playButton = document.querySelector('.gioca');
+
+//aggiunto un evento al bottone per applicare scelte calcoli e funzioni al click
+playButton.addEventListener('click', function(){
+  //faccio scegliere al giocatore pari o dispari come stringa
+  let choice = prompt("Pari o Dispari");
+  console.log("Il giocatore ha scelto : " + choice);
+  //il giocatore sceglie un numero
+  let inputNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+  console.log("Il giocatore ha inserito il numero: " + inputNumber);
+  //il computer sceglie un numero
+  let computerNumber =Math.ceil(Math.random() * (5 - 1 + 1));
+  console.log("Il computer ha inserito il numero: " + computerNumber);
+  //la somma dei due numeri
+  let somma = inputNumber + computerNumber;
+  console.log("La somma : " + somma);
+
+  //chiamo la funzione
+  pariDispari();
+
+  //la funzione che valuta condizioni e opzioni delle variabili
+  function pariDispari(){
+    if(inputNumber < 1 || inputNumber > 5){
+      console.log("Inserisci un numero valido");
+    }
+    if((choice.toLowerCase() === 'pari') && (somma % 2 == 0)){
+      console.log("Il giocatore ha vinto scegliendo pari");
+    } else if ((choice.toLowerCase() === 'dispari') && (somma % 2 == 1)){
+      console.log("Il giocatore ha vinto scegliendo dispari");
+    } else {
+      console.log("Il computer vince");
+    }
+  }
+})
